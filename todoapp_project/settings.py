37bec99d213106875f604e45961a39299fe3fea6
SebastianLoaiza10 <sebastian2003002@gmail.com>
2023-12-20 20:34:24 -0500

    settings changes

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@24^---4vo844invjvnfchu1&xqnhfag0pwhzg6ooz5zm^s^*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['to-do-app-gamma-weld.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
